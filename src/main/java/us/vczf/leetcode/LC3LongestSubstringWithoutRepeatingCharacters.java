0edfb5ed269257f77fda5fca696ddbd9c0b2a6b2
Christian Demsar <christian@git.email.demsar.us>
2022-03-14 23:43:33 -0400

    LC3: Simplify window update with Math.max

--- src/main/java/us/vczf/leetcode/LC3LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/us/vczf/leetcode/LC3LongestSubstringWithoutRepeatingCharacters.java
@@ -12,13 +12,12 @@ class LC3LongestSubstringWithoutRepeatingCharacters {
         while (j < s.length()) {
             char ch = s.charAt(j);
 
-            // Check if {ch} has been seen before. If it has, is it
-            // considered valid by being within the window [i, j]? If it is,
-            // we shrink the window by sliding i over to the index after the
-            // duplicate character.
-            Integer chIndex = seen.get(ch);
-            if (chIndex != null && chIndex >= i) {
-                i = chIndex + 1;
+            // Check if {ch} has been seen before.
+            // We only update the window if chIndex + 1 was already inside it.
+            // Otherwise, it has no effect on the current substring.
+            if (seen.containsKey(ch)) {
+                int chIndex = seen.get(ch);
+                i = Math.max(i, chIndex + 1);
             }
 
             // Store {ch} in {seen}, save size of window if larger, and
